b20e908a3b8e72231dbb6776b8d925f587df2a14
Yorvis <yorvis.melendez@gmail.com>
2023-11-08 21:02:23 -0500

    isTyping funcionandogit add .!

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ io.on("connection", (socket) => {
 
   // Manejar notificaciones de escritura (typing)
   socket.on("typing", ({ user, isTyping }) => {
+    console.log(
+      `Evento de escritura recibido: Usuario ${user}, isTyping: ${isTyping}`
+    );
     if (isTyping) {
       // Emitir un mensaje de escritura a todos los clientes excepto al remitente
       socket.broadcast.emit("typing", { user, isTyping: true });
@@ -49,46 +52,3 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-
-
-// const { conn } = require("./src/db.js");
-// require("dotenv").config();
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// const server = require("./src/server.js");
-
-// const PORT = 3001;
-
-// const httpServer = http.createServer(server);
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// conn.sync({ force: false }).then(() => {
-//   console.log("Base de datos conectada");
-// });
-
-// httpServer.listen(PORT, () => {
-//   console.log(`Servidor iniciado en ${PORT}`);
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`Cliente conectado: ${socket.id}`);
-//   socket.emit("message", "Hola, te has conectado correctamente.");
-
-//   socket.on("disconnect", () => {
-//     console.log(`Cliente desconectado: ${socket.id}`);
-//   });
-
-//   socket.on("chat message", (message) => {
-//     console.log(`Mensaje recibido: ${message}`);
-//     // Emitir el mensaje a todos los clientes, incluido el remitente
-//     io.emit("chat message", message);
-//   });
-// });
